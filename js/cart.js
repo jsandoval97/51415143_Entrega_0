0af5e08d1363d5fb35657fb28710c291c540a7cd
jsandoval97 <88573821+jsandoval97@users.noreply.github.com>
2021-10-21 22:12:25 -0300

    más modificaciones entrega 5
    Quedo pronta la parte de sumar el producto, falta el sumario

--- js/cart.js
+++ js/cart.js
@@ -1,70 +1,84 @@
 var carritoArray = [];
 
 function showCart(){
-    
-    let htmlContentToAppend = "";
 
     for(let i = 0; i < carritoArray.articles.length; i++){
         let carrito = carritoArray.articles[i];
+		let productDiv = document.createElement('div');
+		productDiv.classList.add("row");
 
 
 
-        htmlContentToAppend += `
-				 					<div class="row">
+        productDiv.innerHTML += `
+				 					
 					 					<div class="col-md-3">
-					 						<img class="img-fluid mx-auto d-block image" src="` + carrito.src + `" alt="Sample">
+					 						<img class="img-fluid mx-auto d-block image" src=${carrito.src} " alt="Sample">
 					 					</div>
 					 					<div class="col-md-8">
 					 						<div class="info">
 						 						<div class="row">
 							 						<div class="col-md-5 product-name">
 							 							<div class="product-name">
-								 							<a href="#">` + carrito.name + `</a>
+								 							<a href="#"> ${carrito.name} </a>
 								 							<div class="product-info">
-									 							<div>Costo Unitario: <span class="value" id="moneda">` + carrito.currency + `</span><span class="value" id="precio">`+ " " + carrito.unitCost + `</span></div>
+									 							<div>Costo Unitario: <span class="value" id="moneda"> ${carrito.currency} </span><span class="value" id="precio">`+ " " + carrito.unitCost + `</span></div>
 									 						</div>
 									 					</div>
 							 						</div>
 							 						<div class="col-md-4 quantity">
 							 							<label for="quantity">Cantidad:</label>
-							 							<input id="quantity" type="number" value ="` + carrito.count + `" class="form-control quantity-input">
+							 							<input id="quantity" type="number" value = ${carrito.count} class="form-control quantity-input"> 
 							 						</div>
 							 						<div class="col-md-3 price">
-							 							<span>` + carrito.currency + " " +`</span>` + carrito.unitCost * carrito.count + `<span></span>
+							 							<span> ${carrito.currency} </span><span> ${carrito.unitCost * carrito.count} </span>
 							 						</div>
 							 					</div>
 							 				</div>
 					 					</div>
-					 				</div>
+					 				
 				 				
         `
-        }
+		
+		let inputQuantity = productDiv.getElementsByClassName("quantity-input") [0]
 
-        document.getElementById("cartProduct").innerHTML = htmlContentToAppend;
+		inputQuantity.addEventListener('change', (e)=>{
+
+			productDiv.getElementsByClassName("price") [0].innerHTML = `<span> ${carrito.currency} </span><span> ${carrito.unitCost * e.target.value } </span>`
+		})
+		
+		document.getElementById("cartProduct").appendChild(productDiv);
+
+		}
+        
 
     }
 
+	
 	function showTotal(){
 
 		let htmlContentToAppend = "";
+		let productList = document.getElementById("cartProduct").children;
 
     	for(let i = 0; i < carritoArray.articles.length; i++){
-        	let carrito2 = carritoArray.articles[i];
+        	let product = carritoArray.articles[i];
+			let cantProduct = productList[i].getElementsByClassName("quantity-input") [0];
+	
 
-			let precioCompra = parseInt(carrito2.unitCost) * (document.getElementById("quantity").value);
+			let precioCompra = parseFloat(product.unitCost) * parseFloat(cantProduct);
 
         	htmlContentToAppend +=   `
 
 			<h3>Sumario</h3>
-                    <div class="summary-item"><span class="text">Subtotal</span><span class="price">` + precioCompra + `</span></div>
+                    <div class="summary-item"><span class="text">Subtotal</span><span type="number" class="price">${precioCompra}</span></div>
                     <div class="summary-item"><span class="text">Envío</span><span class="price">$0</span></div>
-                    <div class="summary-item"><span class="text">Total</span><span class="price">` + precioCompra + `</span></div>
+                    <div class="summary-item"><span class="text">Total</span><span class="price">${precioCompra}</span></div>
                     <button type="button" class="btn btn-primary btn-lg btn-block">Pagar</button>
 
 					`
 		}
 
 		document.getElementById("cuentas").innerHTML = htmlContentToAppend;
+
 	}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
